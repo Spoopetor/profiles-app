34199d5c5b39a3c31c078168cde02757bd2ea2ca
Andy Malak <andymalak1@gmail.com>
2025-11-07 13:54:06 -0500

    Removed visible ID from ProfileCard.jsx

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -5,7 +5,7 @@ export default function ProfileCard({ name, likes, id, likeHandler }) {
   return (
     <Card className="mb-3 shadow-sm">
       <Card.Body>
-        <Card.Title className="h5 mb-1">{name} : {id}</Card.Title>
+        <Card.Title className="h5 mb-1">{name}</Card.Title>
         <Card.Text className="mb-0">Likes: {likes}</Card.Text>
         <Button variant="primary" onClick={()=> likeHandler(id)}>
           Like
